e32df413d86fc3a9f0bd95813a05f849e1d6d3db
Rui Pires <pires.rui@outlook.pt>
2021-12-23 17:17:39 +0000

    Setup

--- server/server.c
+++ server/server.c
@@ -19,11 +19,14 @@ void parse_args(char *dsport, bool *verbose, int argc, char *argv[]){
   for (int i = 1; i < argc; i++){
     if (strcmp(argv[i], "-v") == 0)
       *verbose = TRUE;
-    else if (strcmp(argv[i], "-p") == 0)
+    else if (strcmp(argv[i], "-p") == 0){
+      ASSERT(argc != i + 1, "-p requires another argument");
       strcpy(dsport, argv[++i]);
-    else throw_error("Unkown command line argument");
+
+    }else throw_error("Unkown command line argument");
   }
 
+  DEFAULT(dsport, DEFAULT_DSPORT);
 
   DEBUG_MSG("verbose set to: %d\n", *verbose);
   DEBUG_MSG("dsport set to: %s\n", dsport);
