e69934cdf69a0abc115ed2941caf7e9e65769c87
aritaduarte <anaritard.2000@gmail.com>
2022-01-08 11:05:32 -0800

    Added my_groups

--- server/commands.c
+++ server/commands.c
@@ -331,9 +331,86 @@ void unsubscribe(connection_context_t *connection, char *args, char *fs){
     send_udp_message(connection, msg_buffer);
 }
 
-void my_groups(connection_context_t *temp1, char *temp2, char *temp3){}
+void my_groups(connection_context_t *connection, char *args, char *fs){
+  char *uid = get_word(&args);
 
+  char *groups_path = (char *) malloc(sizeof(char)*(strlen(fs) + strlen(SERVER_GROUPS_NAME) + 3));
+  sprintf(groups_path, "%s/%s/", fs, SERVER_GROUPS_NAME);
 
+  sll_link_t groups_list = list_subdirectories(groups_path);
+
+  int n_subscribed_groups = 0;
+
+  char *groups_buffer;
+
+  char n_str[BUFFER_SIZE];
+
+  FOR_ITEM_IN_LIST(char* group, groups_list)
+
+    char *group_path = (char *) malloc(sizeof(char) * (strlen(groups_path) + strlen(group) + 2));
+    
+    sprintf(group_path, "%s/%s", groups_path, group);
+
+    if (file_exists(group_path, uid)){
+
+      n_subscribed_groups++;
+
+      if(n_subscribed_groups == 1){
+
+        groups_buffer = (char *) malloc(sizeof(char)*(33));
+
+      }else{
+
+        groups_buffer = (char *) realloc(groups_buffer, sizeof(char)*(33*n_subscribed_groups));
+
+      }
+
+      char *top = &groups_buffer[strlen(groups_buffer)];
+
+      char *group_msgs_path = (char *) malloc(sizeof(char)*(strlen(groups_path) + strlen(group) + 3));
+
+      sprintf(group_msgs_path, "%s/%s/", groups_path, group);
+      sll_link_t msg_list = list_subdirectories(group_msgs_path);
+
+      char *name_path = (char *) malloc(sizeof(char) * (strlen(group_msgs_path) + strlen("name.txt") + 3));
+      sprintf(name_path, "%s/%s", group_msgs_path, "name.txt");
+      FILE *file = fopen(name_path, "r");
+      free(name_path);
+
+      char group_name[25];
+
+      fscanf(file, "%s", group_name);
+      fclose(file);
+
+      sprintf(top, " %s %s %04d", group, group_name, sll_size(msg_list));
+
+      sll_destroy(&msg_list);
+      free(group_msgs_path);
+  
+
+      }
+      
+  free(group_path);
+
+  END_FIIL()
+
+  char *msg_buffer = (char *) malloc(sizeof(char)*(4 + n_subscribed_groups + sizeof(groups_buffer)));
+
+  sprintf(n_str, "%ld", n_subscribed_groups);
+
+  sprintf(msg_buffer, "RGM %s%s", n_str, groups_buffer);
+
+  send_udp_message(connection, msg_buffer);
+
+  free(groups_buffer);
+
+  free(msg_buffer);
+
+  free(groups_path);
+
+  sll_destroy(&groups_list);
+
+}
 
 void ulist(connection_context_t *connection, char *fs){
     char gid[3];
